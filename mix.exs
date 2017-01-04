3d6030c292fe2122c11516c3f37960217725df69
Garry Hill <garry@magnetised.net>
2017-01-04 10:05:50 +0000

    Include missing pipe app

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Sqlite.Ecto.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    [applications: [:logger, :ecto, :sqlitex, :poison]]
+    [applications: [:logger, :ecto, :sqlitex, :poison, :pipe]]
   end
 
   # Dependencies
